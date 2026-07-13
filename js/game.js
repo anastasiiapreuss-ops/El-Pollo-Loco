50f78d0051f807c333955d436a6b26d961ad93d4
Anastasiia Preuß <anastasiiapreuss@gmail.com>
2026-07-13 14:40:33 +0200

    add: rename docs, add js, models, style.css

---
+++ js/game.js
@@ -0,0 +1,12 @@
+let canvas;
+let ctx;
+let character = new Character();
+let enemies
+
+function init(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    console.log('My charakter is', character);
+    
+}
+
