aed647b7bf255bf75631bcf6a072a364cb9d12fb
Anastasiia Preuß <anastasiiapreuss@gmail.com>
2026-07-14 16:59:58 +0200

    add: background_img, cloud animation, Pepe animation, Chicken + Pepe position, size

--- js/models/movable-objekt.class.js
+++ js/models/movable-objekt.class.js
@@ -4,11 +4,26 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageChace = {};
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
+    /**
+     * 
+     * @param {Array} arr 
+     */
+    
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageChace[path] = img;
+        });
+    }
+
 
     moveRight() {
         console.log('Moving right!');
